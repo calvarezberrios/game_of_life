4fdd6a096e2a6093d724b1b9e30e99011ffd0642
Carlos 'Mannie' Alvarez <carlos.alvarezberrio@gmail.com>
2020-10-19 07:49:00 -0400

    testing changes

--- src/utils/findNeighbors.js
+++ src/utils/findNeighbors.js
@@ -10,28 +10,28 @@ const operations = [
 ]
 
 export default function findNeighbors(grid, c, r) {
-    let rowLimit = grid.length-1;
-    let colLimit = grid[0].length-1;
+    // let rowLimit = grid.length-1;
+    // let colLimit = grid[0].length-1;
 
     let neighbors = 0;
     
-    for(let x = Math.max(0, c - 1); x <= Math.min(c + 1, rowLimit); x++) {
-        for(let y = Math.max(0, r - 1); y <= Math.min(r + 1, colLimit); y++) {
-            if((x !== c || y !== r) && grid[x][y].isAlive) {
-                neighbors += 1
-            }
-        }
-    }
+    // for(let x = Math.max(0, c - 1); x <= Math.min(c + 1, rowLimit); x++) {
+    //     for(let y = Math.max(0, r - 1); y <= Math.min(r + 1, colLimit); y++) {
+    //         if((x !== c || y !== r) && grid[x][y].isAlive) {
+    //             neighbors += 1
+    //         }
+    //     }
+    // }
 
     
 
-    // operations.forEach(([x, y]) => {
-    //     const newI = c + x;
-    //     const newJ = r + y;
-    //     if(newI >= 0 && newI < grid.length && newJ >= 0 && newJ < grid[0].length) {
-    //         neighbors += 1;
-    //     }
-    // })
-    // console.log(neighbors)
+    operations.forEach(([x, y]) => {
+        const newI = c + x;
+        const newJ = r + y;
+        if((newI >= 0 && newI < grid.length) && (newJ >= 0 && newJ < grid[0].length) && grid[newI][newJ].isAlive) {
+            neighbors += 1;
+        }
+    })
+    
     return neighbors;
 }
